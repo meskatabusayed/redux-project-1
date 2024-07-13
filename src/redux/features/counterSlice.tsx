da18aed6b005982c0f8ef6b0606c8dbf77d1e715
meskatabusayed <meskatabusayed@gmail.com>
2024-07-13 11:08:49 +0530

    counterSlice added

---
+++ src/redux/features/counterSlice.tsx
@@ -0,0 +1,7 @@
+import { createSlice} from "@reduxjs/toolkit";
+const initialState = {counter : 0}
+const counterSlice = createSlice({
+    name : "counter",
+    initialState,
+    reducers : {}
+});
